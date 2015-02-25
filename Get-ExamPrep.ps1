f02bb9a9079eb0c415ec144932913ca8700f5b7c
Maxime Fortier <fortier.maxime@gmail.com>
2015-02-25 10:17:34 -0500

    Uploading old but reusable material
    Uploading the PowerShell Script to retrieve the Exam Prep Sessions from
    Channel 9. Sessions from Microsoft TechEd 2014 - North America

---
+++ Get-ExamPrep.ps1
@@ -0,0 +1,37 @@
+﻿###############################################################
+#
+#    Maxime Fortier QuickWins - Download Channel 9 Exam Prep
+#    Get-ExamPrep.ps1
+#    
+#
+#    By Maxime Fortier
+#
+#    Script downloads Exam Preparation sessions from Channel 9
+#    Sessions are from Microsoft TechEd 2014 North America.
+#    http://channel9.msdn.com/
+#
+#    Simply run Get-ExamPrep.ps1
+#
+###############################################################
+
+
+$MyDocumentsPath = [Environment]::GetFolderPath("MyDocuments")
+$ExamPrepFolder = $MyDocumentsPath + "\Microsoft TechEd 2014 North America - Exam Prep Sessions"
+if(!(Test-Path -Path $ExamPrepFolder )){
+    New-Item -ItemType directory -Path $ExamPrepFolder
+}
+$i = 1..16 ; ForEach ($SessionID in $i)
+{
+	if ($SessionID -le 9)
+	{
+	    $SessionID = "0" + $SessionID
+	}   
+    $ExamPrepURL = "http://video.ch9.ms/sessions/teched/na/2014/EXM" + $SessionID + ".mp4"
+    Write-Host "Downloading the Channel 9 session from $ExamPrepURL"
+    
+    $ExamPrepFile = $ExamPrepFolder + "\EXM" + $SessionID + ".mp4"
+    
+    Invoke-WebRequest $ExamPrepURL -OutFile $ExamPrepFile
+}
+
+
